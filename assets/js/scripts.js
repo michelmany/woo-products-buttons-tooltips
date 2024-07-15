c1b38dfb46e370f1962825003e16d6122a64df60
michelmany <hello@michelmany.com>
2024-07-15 15:52:02 +0200

    feat: add initial changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,51 @@
 (function($) {
 
     $(document).ready(function() {
-        let cache = {};
-        let debounceTimer;
         let sku;
         let anchorSelector;
+
+        let productDetails = {};
+        let currentPage = 1;
+        let isLoading = false;
+        let skus = [];
+        let tooltips = [];
+
+        function loadProductDetails(page, skus, elem = null) {
+            if (isLoading) return;
+            isLoading = true;
+
+            $.ajax({
+                method: 'POST',
+                url: ajax_var.url,
+                data: {
+                    action: ajax_var.action,
+                    nonce: ajax_var.nonce,
+                    page: page,
+                    skus: skus,
+                },
+                success: function(response) {
+                    if (response.success) {
+                        $.extend(productDetails, response.data);
+                        currentPage++;
+
+                        console.log(response.data);
+
+                        // console.log('propd:', productDetails)
+
+                        if (productDetails[sku] && elem) {
+                            $(elem).html(productDetails[sku].price);
+                        }
+                    } else {
+                        console.error('Failed to fetch product details.');
+                    }
+                    isLoading = false;
+                },
+                error: function() {
+                    console.error('AJAX request failed.');
+                    isLoading = false;
+                }
+            });
+        }
 
         const pagesSkuInTheButton = [
             'page-id-20403',
@@ -21,53 +62,80 @@
             return $('body').hasClass(className);
         });
 
-        if(isPageWithSkyInTheButton) {
-            anchorSelector = '.apply-labkings-tooltips .wpb_text_column table td a';
-        } else {
-            anchorSelector = '.apply-labkings-tooltips .wpb_single_image + .wpb_text_column table td + td a';
-        }
+        let regularPagesSelector = '.apply-labkings-tooltips .wpb_text_column table td a';
+        let pagesWithSkuSelector = '.apply-labkings-tooltips .wpb_single_image + .wpb_text_column table td + td a';
+
+        anchorSelector = isPageWithSkyInTheButton ? regularPagesSelector : pagesWithSkuSelector;
 
         $(anchorSelector).each(function() {
             $(this).prepend('<div class="tooltip">Loading price...</div>');
         });
 
+        function checkViewport() {
+
+            $(anchorSelector).each(function() {
+                let rect = this.getBoundingClientRect();
+                let isInViewport = rect.top < window.innerHeight && rect.bottom >= 0;
+
+                let $tdWrapper = $(this).parent();
+                let $tdBefore = $tdWrapper.prev();
+
+                sku = isPageWithSkyInTheButton
+                    ? $(this).find('button').text().toString().trim()
+                    : $tdBefore.text().toString().trim();
+
+                if (isInViewport) {
+                    if (!skus.includes(sku)) {
+                        skus.push(sku);
+                    }
+                } else {
+                    const index = skus.indexOf(sku);
+                    if (index > -1) {
+                        skus.splice(index, 1);
+                    }
+                }
+            });
+
+            console.log(skus);
+
+            if (skus.length > 0) {
+                console.log('loading from viewport');
+                loadProductDetails(currentPage, skus);
+            }
+        }
+
+        // Initial check
+        checkViewport();
+
+        // Check on scroll
+        let debounceTimeout;
+        $(window).on('scroll', function() {
+            clearTimeout(debounceTimeout);
+            debounceTimeout = setTimeout(checkViewport, 300);
+        });
+
+
         $(anchorSelector).on('mouseenter', function() {
             let $tdWrapper = $(this).parent();
             let $tdBefore = $tdWrapper.prev();
+            let $tooltip = $tdWrapper.find('div.tooltip');
 
-            if (isPageWithSkyInTheButton) {
-                sku = $(this).find('button').text().toString().trim();
-            } else {
-                sku = $tdBefore.text().toString().trim();
+            sku = isPageWithSkyInTheButton
+                ? $(this).find('button').text().toString().trim()
+                : $tdBefore.text().toString().trim();
+
+            if (isLoading) {
+                $tooltip.html('Loading price...');
             }
 
-            if (cache[sku]) {
-                $tdWrapper.find('div.tooltip').html(cache[sku]);
-            } else {
-                clearTimeout(debounceTimer);
-                debounceTimer = setTimeout(function() {
-                    console.time('AJAX Request'); // Start timing here
-                    axios({
-                        method: 'post',
-                        url: ajax_var.url,
-                        data: {
-                            action: ajax_var.action,
-                            nonce: ajax_var.nonce,
-                            sku: sku,
-                        },
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                        },
-                    }).then(function(response) {
-                        console.timeEnd('AJAX Request'); // End timing here
-                        cache[sku] = response.data;
-                        $tdWrapper.find('div.tooltip').html(response.data);
-                    }).catch(function(error) {
-                        console.log(error);
-                    });
-                }, 300);
+            if (!productDetails[sku]) {
+                console.log('loading from mouseeneter')
+                loadProductDetails(currentPage, skus, $tooltip);
+            } else if (productDetails[sku]) {
+                $tooltip.html(productDetails[sku].price);
             }
         });
+
     });
 
 })(jQuery);
